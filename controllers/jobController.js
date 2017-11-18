062e594b66afc2bf75cd5bb68600417db72fb550
kjsuyo <33008884+kjsuyo@users.noreply.github.com>
2017-11-18 16:25:30 -0500

    sample

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,67 +1,20 @@
-var Job = require('../models/job');
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
 
 var async = require('async');
 
-exports.index = function(req, res) {
-
-    async.parallel({
-        job_count: function(callback) {
-          sequelize.fn('count', { industry: 'business' });
-        },
-/*        job_instance_count: function(callback) {
-            JobInstance.count(callback);
-        },
-        job_instance_available_count: function(callback) {
-            JobInstance.count({status:'Available'},callback);
-        },
-
-        author_count: function(callback) {
-            Author.count(callback);
-        },
-        genre_count: function(callback) {
-            Genre.count(callback);
-        },
-        */
-    }, function(err, results) {
-        res.render('index', { title: 'Skillterrier', error: err, data: results });
-    });
-};
-
-
-// Display list of all books
-exports.book_list = function(req, res, next) {
-
-  Book.find({}, 'title author ')
-    .populate('author')
-    .exec(function (err, list_books) {
-      if (err) { return next(err); }
-      //Successful, so render
-      res.render('book_list', { title: 'Book List', book_list:  list_books});
-    });
-
-};
-
-// Display detail page for a specific book
-exports.book_detail = function(req, res, next) {
-
-    async.parallel({
-        book: function(callback) {
-
-            Book.findById(req.params.id)
-              .populate('author')
-              .populate('genre')
-              .exec(callback);
-        },
-        book_instance: function(callback) {
-
-          BookInstance.find({ 'book': req.params.id })
-          //.populate('book')
-          .exec(callback);
-        },
-    }, function(err, results) {
-        if (err) { return next(err); }
-        //Successful, so render
-        res.render('book_detail', { title: 'Title', book:  results.book, book_instances: results.book_instance } );
-    });
+module.exports = {
+  list(req, res) {
+    return Job
+      .findAll({
+        include: [{
+          model: Job,
+          as: 'Jobs',
+        }],
+      })
+      .then((jobs) => res.status(200).send(jobs))
+      .catch((error) => res.status(400).send(error));
+  },
 
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
+var jobs_controller = require('../controllers/jobController');
 
 
 router.get('/', function(req, res) {
